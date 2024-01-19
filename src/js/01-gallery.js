42b1a1d0f857345546e9aaa6cc9042b6453c7a7c
EugenAd <bosmaneugen@gmail.com>
2024-01-19 20:05:06 +0200

    Finished task  01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Descris în documentație
+import SimpleLightbox from 'simplelightbox';
+// Import suplimentar de stil
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+const createGalleryItems = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" data-src="${original}" alt="${description}"/>
+       </a>
+      </li> `
+  )
+  .join('');
+galleryList.insertAdjacentHTML('afterbegin', createGalleryItems);
+const galleryHandler = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+galleryHandler.on('show.SimpleLightbox');
 
 console.log(galleryItems);
