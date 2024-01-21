24d9392b43fe4b5364923a75b7b1c7b31c1bdd61
EugenAd <bosmaneugen@gmail.com>
2024-01-21 17:34:46 +0200

    Finished task 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
+player.on(
+  'timeupdate',
+  _.throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+window.addEventListener('load', () => {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+});
